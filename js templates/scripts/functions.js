e016a3ef59a0fd9a00d86006ba59a4af5299d973
shruthi093 <jayasruthi.l@gmail.com>
2022-07-20 15:49:00 +0530

    JS - day 3

--- js templates/scripts/functions.js
+++ js templates/scripts/functions.js
@@ -8,14 +8,49 @@ greetUser("peter")*/
 /*var greetFunction=greetUser
 
 greetFunction('greetfunction')
-greetUser('greetuser')*/
+greetUser('greetuser')
 
 var greetFunction=function(fname){
     console.log(`Welcome OnBoard ${fname}`) 
 }
-greetFunction('greetfunction')
+greetFunction('greetfunction')*/
 
 
+//default parameters
 
+/*function greetUser(mobile,reg,fname='User'){//write non-default args first
+//fname=fname??"User"
+console.log(`Welcome ${fname} : ${mobile} :${reg}`)
+}
+greetUser(92839283,"BE1239283")
 
-
+function userList(userData){
+userData.mobile=9283082
+return userData
+}
+let user={
+    fname:"john",
+    email:"john@gmail.com"
+}
+user=userList(user)
+console.log(user)
+function userList(userData){
+    //destructing with alias name
+    let {fname:fullname,useremail,usermobile}=userData
+   console.log(fullname)
+}
+let user={
+        fname:"john",
+        email:"john@gmail.com",
+        mobile:2938293
+}
+//object destructing
+let {fname,email,mobile}=user
+console.log(fname)
+let arr=[90,"john",28372,"IND",{role:"admin",company:"XYZ"},[348938,3984389]]
+let [marks,fname,pin,country,{role,company},[x,y]]=arr
+console.log(x,y)
+let arr=[20,"john","john@gmail.com"]
+//let [age,[userdetails,email]]=arr
+let [age,...userdetails]=arr // rest/spread operator
+console.log(userdetails)*/
---
+++ js templates/scripts/operators.js
@@ -0,0 +1,33 @@
+/*let a=null
+console.log(a)*/
+/*a!==b?console.log("not equal"):console.log("equal")
+function sample(){
+    console.log("sample")
+}
+console.log(sample())
+let res=sample()
+sample()*/
+
+/*let a;
+if(a){
+console.log(a)
+}
+else{
+    a=10
+    console.log(a)
+}
+let a;
+let b=null; 
+
+x=a??b??20//looks for first defined value
+y=a||b||20 // looks for first truthy value
+
+console.log(x)
+console.log(y)
+let num=+"10"//number type conversion
+console.log(typeof(num))*/
+
+
+
+
+
